ffee5a9268f1336a7719e610025da1f89e341a2d
Galo Izquierdo V <36722870+gizquier@users.noreply.github.com>
2026-04-29 10:03:20 -0500

    Add properties and methods to ModeloMascota
    Added fields and corresponding getters and setters for id, nombre, raza, especie, and clienteId.

--- src/main/java/com/grupo2/sistemadegestionveterinaria/modelo/ModeloMascota.java
+++ src/main/java/com/grupo2/sistemadegestionveterinaria/modelo/ModeloMascota.java
@@ -7,4 +7,50 @@ package com.grupo2.sistemadegestionveterinaria.modelo;
 
 public class ModeloMascota {
     
+  private Integer id;
+  private String nombre;
+  private String raza;
+  private String especie;
+  private int clienteId;
+
+  // getters y setters
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getRaza() {
+    return raza;
+  }
+
+  public void setRaza(String raza) {
+    this.raza = raza;
+  }
+
+  public String getEspecie() {
+    return especie;
+  }
+
+  public void setEspecie(String especie) {
+    this.especie = especie;
+  }
+
+  public int getClienteId() {
+    return clienteId;
+  }
+
+  public void setClienteId(int clienteId) {
+    this.clienteId = clienteId;
+  }
 }
