4b9440f71902c80df9a7ccfac47bc03d14b13861
Mario Jacho <110362059+mmjacho@users.noreply.github.com>
2026-06-24 22:10:36 -0500

    update module

--- src/main/java/com/grupo2/sistemadegestionveterinaria/SistemaDeGestionVeterinaria.java
+++ src/main/java/com/grupo2/sistemadegestionveterinaria/SistemaDeGestionVeterinaria.java
@@ -4,11 +4,25 @@
  */
 package com.grupo2.sistemadegestionveterinaria;
 
-import com.grupo2.sistemadegestionveterinaria.vista.VistaMenuPrincipal;
+import com.grupo2.sistemadegestionveterinaria.controlador.ControladorUsuario;
+import com.grupo2.sistemadegestionveterinaria.modelo.ModeloUsuario;
+import com.grupo2.sistemadegestionveterinaria.vista.VistaLogin;
+import javax.swing.SwingUtilities;
 
 public class SistemaDeGestionVeterinaria {
 
-  public static void main(String[] args) {
-    new VistaMenuPrincipal().setVisible(true);
-  }
+    public static void main(String[] args) {
+        // Ejecución segura de la interfaz gráfica apuntando al flujo del Login
+        SwingUtilities.invokeLater(() -> {
+            // 1. Instanciamos el Modelo y la Vista del Módulo 0
+            VistaLogin vistaLogin = new VistaLogin();
+            ModeloUsuario modeloUsuario = new ModeloUsuario();
+            
+            // 2. Acoplamos los componentes mediante el Controlador oficial
+            ControladorUsuario controlador = new ControladorUsuario(modeloUsuario, vistaLogin);
+
+            // 3. Desplegamos la ventana de acceso
+            vistaLogin.setVisible(true);
+        });
+    }
 }
